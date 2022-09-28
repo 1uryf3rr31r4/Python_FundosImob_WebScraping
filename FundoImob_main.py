c6e9695c84be2659b986fef715baa5e361dc72b4
Iury Ferreira <1uryf3rr31r4@gmail.com>
2022-09-28 20:54:54 -0300

    Print do Preco e Ultimo Rendimento

--- FundoImob_main.py
+++ FundoImob_main.py
@@ -4,3 +4,11 @@ from bs4 import BeautifulSoup
 site = "https://www.fundsexplorer.com.br/funds/mxrf11"
 
 html = requests.get(site).content
+
+dados = BeautifulSoup(html, 'html.parser')
+
+preco = dados.find("span", class_ = "price")
+ultimo_rendimento = dados.find_all("span", class_ = "indicator-value")
+
+print("Preço:", preco.text[19:])
+print("Último Rendimento:", ultimo_rendimento[1].text[17:])
